fixed the loss calculation too - wasn't an issue, but not clean

dynamicmatching/bellman.py
@@ -229,8 +229,7 @@ def match_moments(xi, theta, tPs, tQs,
            #ss_star[i, ] = ss_cur
            #ss_cur = walker(ss_cur)
 
-    resid = torch.square(tMuHat - mu_star).sum()
-    # this exagerates loss bc mu_star is zero if train0 = False. Shouldn't matter, constant shift.
+    resid = torch.square(tMuHat[idx0:,:,:] - mu_star[idx0,:,:]).sum()
 
     torch.cuda.empty_cache()
 