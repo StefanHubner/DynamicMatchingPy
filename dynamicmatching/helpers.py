a0bcc2bde508a2ca5bfbf9195cca4b6499645344
Stefan Hubner <stefan.hubner@economics.ox.ac.uk>
2025-12-04 16:49:47 +0000

    3 to 4

--- dynamicmatching/helpers.py
+++ dynamicmatching/helpers.py
@@ -122,7 +122,7 @@ def tauMScal(par, t, d, dev):
         d * mbasis(3, 2, 2, dev),
         d * mbasis(3, 2, 1, dev),
         d * mbasis(3, 1, 2, dev)))
-    psi = torch.tensor([-0.0953] * 3, device = dev)
+    psi = torch.tensor([-0.0953] * 4, device = dev)
     p_const = torch.cat([ par[[0, 1, 2, 3, 4]], psi ]).view(-1, 1, 1)
     const = torch.multiply(b_const, p_const).sum(dim = 0)
     return extend(const)
